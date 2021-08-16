3a52dd449c7e66f79f7de7f302f25184a2e3df8b
SashaEmWork <oem@tsukat.com>
2021-08-16 16:01:48 +0300

    Project added

---
+++ RedBallCustomControl/Assets/_Project/InputManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class InputManager : MonoBehaviour
+{
+    private enum InputType
+    {
+        UI,
+        Analogue
+    }
+
+    [SerializeField] private InputType _inputType;
+    [SerializeField] private VisualInput _uiInputCanvas;
+    [SerializeField] private Rigidbody _sphere;
+
+    void Awake()
+    {
+        InitializeInputType(_inputType);
+    }
+
+    void InitializeInputType(InputType InputType)
+    {
+        switch(InputType)
+        {
+            case InputType.UI:
+            {
+                InitializeUI();
+                return;
+            }
+            case InputType.Analogue:
+            {
+                InitializeAnalogue();
+                return;
+            }
+        }
+    }
+
+    void InitializeUI()
+    {
+        _uiInputCanvas = Instantiate(_uiInputCanvas);
+        _uiInputCanvas.Button.onClick.AddListener(TurnOnGravity);
+    }
+
+    void InitializeAnalogue()
+    {
+        Debug.Log("I don't know how to use arduino.");
+    }
+
+    private void TurnOnGravity()
+    {
+        _sphere.useGravity = true;
+    }
+
+    
+}
---
+++ RedBallCustomControl/Assets/_Project/VisualInput/VisualInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualInput : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+    public Button Button => _button;
+}
